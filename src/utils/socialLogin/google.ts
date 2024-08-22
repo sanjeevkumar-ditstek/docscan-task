c584499d3c2c0881855fadb3b35ed0fc45fcb407
sanjeev <sanjeev.kumar@ditstek.com>
2024-08-22 14:40:05 +0530

    profile api using axios for google login

--- src/utils/socialLogin/google.ts
+++ src/utils/socialLogin/google.ts
@@ -1,5 +1,5 @@
 import { OAuth2Client } from 'google-auth-library';
-
+import axios from 'axios';
 // Replace with your actual Google Client ID
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 
@@ -15,22 +15,30 @@ interface VerifyResult {
 
 /**
  * Verifies a Google ID token.
- * @param {string} idToken - The ID token to verify.
+ * @param {string} accessToken - The ID token to verify.
  * @returns {Promise<VerifyResult>} - A promise that resolves to the verification result.
  */
 export async function verifyGoogleToken(
-  idToken: string
+  accessToken: string
 ): Promise<VerifyResult> {
   try {
     // Verify the ID token
-    const ticket = await client.verifyIdToken({
-      idToken: idToken,
-      audience: CLIENT_ID // Specify the CLIENT_ID of the app that accesses the backend
+    const ticket = await client.getTokenInfo(accessToken);
+    const response = await axios.get(process.env.USERINFO_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
     });
-    // Get the payload (user information) from the verified token
-    const payload = ticket.getPayload();
 
-    // Return the user information
+    // Extract profile information from the response
+    const profile = response.data;
+    const payload = {
+      email: ticket?.email,
+      sub: ticket?.sub,
+      given_name: profile?.given_name,
+      family_name: profile?.family_name
+    }
+    // // Return the user information
     return {
       valid: true,
       payload: payload
